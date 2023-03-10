93d92fea89cc9a27483688e84c278d7205d33cb3
A3aaan <andreas.vanderdonckt@student.kuleuven.be>
2023-03-11 00:51:13 +0100

    taalmodel python trials

---
+++ NLTK/distance.py
@@ -0,0 +1,22 @@
+from time import time
+start = time()
+import BERTSimilarity.BERTSimilarity as bertsimilarity
+t = time()
+print("imported", t-start)
+b =bertsimilarity.BERTSimilarity()
+t = time()
+print("module made", t-start)
+
+str1 = "simon says he is good at programming"
+str2 = "simon says he likes programming"
+str3 = "programming is what simon likes"
+
+dist = b.calculate_distance(str1,str2)
+t = time()
+print("distance1 calculated", t-start)
+print(dist)
+
+dist2 = b.calculate_distance(str2,str3)
+t = time()
+print("distance1 calculated", t-start)
+print(dist)
