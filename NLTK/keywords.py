b89600b4912ac36518070842974b0af7233ee1d0
A3aaan <andreas.vanderdonckt@student.kuleuven.be>
2023-03-11 04:47:29 +0100

    please anyone fix, should read files that are too large to read

--- NLTK/keywords.py
+++ NLTK/keywords.py
@@ -1,8 +1,10 @@
 # script.py
 from keybert import KeyBERT
 kw_model = KeyBERT()
+import csv
+
 #language = input("language e/n")
-language = "n"
+language = "e"
 
 if language == 'e':
     print("engels")
@@ -12,25 +14,29 @@ elif language == 'n':
     f1 = open("stop_words_nederlands.txt",'r')
 
 #stopwoordenfile
-stringSwords = f2.read()
+stringSwords = f1.read()
 stringSwords = stringSwords.replace("\n"," ")
 swords = stringSwords.split(" ")
-f2.close()
+f1.close()
 
 
 comments = "full_text.txt"
 #comments = input("type filename")
 
-def my_read(file, size=2048):
-    while 1:
-        data = file.read(size)
-        if not data:
-            break
-        yield data
 
+with open(comments,'r',encoding='iso-8859-1') as fd:
+    csvreader = csv.reader(fd)
 
-with open(comments) as f:
-    for piece in my_read(f):
-        keywords = kw_model.extract_keywords(piece)
+    currentbufferstring = ""
+    keywords = []
 
-print(kw_model.extract_keywords(doc, keyphrase_ngram_range=(2, 4), stop_words=swords))
+    for row in csvreader:
+        try:
+            currentbufferstring = currentbufferstring + row[0]
+        except:
+            test = kw_model.extract_keywords(currentbufferstring)
+            print(kw_model.extract_keywords(currentbufferstring, keyphrase_ngram_range=(2, 4), stop_words=swords))
+            currentbufferstring = ""
+
+    print(keywords)
+
