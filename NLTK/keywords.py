d7462087f98109f124836e3cb2b316ac9de816cd
A3aaan <andreas.vanderdonckt@student.kuleuven.be>
2023-03-11 16:00:34 +0100

    ""

--- NLTK/keywords.py
+++ NLTK/keywords.py
@@ -3,58 +3,62 @@ from keybert import KeyBERT
 kw_model = KeyBERT()
 import csv
 
-#language = input("language e/n")
-language = "e"
-
-if language == 'e':
-    print("engels")
-    f1 = open("stop_words_engels.txt",'r')
-elif language == 'n':
-    print("nederlands")
-    f1 = open("stop_words_nederlands.txt",'r')
-
-#stopwoordenfile
-stringSwords = f1.read()
-stringSwords = stringSwords.replace("\n"," ")
-swords = stringSwords.split(" ")
-f1.close()
-
-lengte_range = [3,5]
-
-comments = "full_text.txt"
-#comments = input("type filename")
-
-
-with open(comments,'r',encoding='iso-8859-1') as fd:
-    csvreader = csv.reader(fd)
-
-    currentbufferstring = ""
-    keywords = set()
-    lines = 0
-
-    for row in csvreader:
-        try:
-            currentbufferstring = currentbufferstring + " " + row[0]
-            lines += 1
-            if lines > 100:
-                lines = 0
-                
-                for element in (kw_model.extract_keywords(currentbufferstring, keyphrase_ngram_range=(lengte_range[0],lengte_range[1]
-), stop_words=swords)):
-                    keywords.add(element)
-                currentbufferstring = ""
-
-        except:
-            test = kw_model.extract_keywords(currentbufferstring)
-            for element in (kw_model.extract_keywords(currentbufferstring, keyphrase_ngram_range=(lengte_range[0],lengte_range[1]
-), stop_words=swords)):
-                    keywords.add(element)
-            currentbufferstring = ""
+def get_keywords(tekst_name,language="e"):
+    #language = "e/n")
     
-    for element in (kw_model.extract_keywords(currentbufferstring, keyphrase_ngram_range=(lengte_range[0],lengte_range[1]
-), stop_words=swords)):
-        keywords.add(element)
+    if language == 'e':
+        print("engels")
+        f1 = open("stop_words_engels.txt",'r')
+    elif language == 'n':
+        print("nederlands")
+        f1 = open("stop_words_nederlands.txt",'r')
+
+    #stopwoordenfile
+    stringSwords = f1.read()
+    stringSwords = stringSwords.replace("\n"," ")
+    swords = stringSwords.split(" ")
+    f1.close()
+    #print("stopwoorden gelezen")
+
+    
+    with open(tekst_name,'r',encoding='iso-8859-1') as fd:
+        csvreader = csv.reader(fd)
+        #print("file geopend")
         currentbufferstring = ""
-    
-    print(keywords)
+        min,max = 3,4
+        keywords = []
+        lines = 0
+        
+
+        #uses Bert to find keywords in the comments
+        for row in csvreader:
+            try:
+                currentbufferstring = currentbufferstring + " " + row[0]
+                lines += 1
+                print(" ",end="")
+                if lines > 100:
+                    lines = 0
+                    addkeywords(keywords,currentbufferstring,min,max,swords)
+                    currentbufferstring = ""
+                    print(".",end= "")
+
+            except:
+                addkeywords(keywords,currentbufferstring,min,max,swords)
+                currentbufferstring = ""
+                print("*",end= "")
+        
+        
+        addkeywords(keywords,currentbufferstring,min,max,swords)
+        currentbufferstring = ""
+        print("_",end= "")
+        
+        keywords.sort(key = lambda x: x[1],reverse=True)
+        return keywords
+        
+
+def addkeywords(keywords,string,min,max,stopwords):
+    for element in (kw_model.extract_keywords(string, keyphrase_ngram_range=(min,max), stop_words=stopwords)):
+        keywords.append(element)
+
+print(get_keywords("bad_reviews_from_last_week.txt","e"))
 
