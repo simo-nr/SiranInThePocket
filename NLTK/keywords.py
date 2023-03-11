22a6eeefdc6aa07fd6a0dd5c2e7314d910efeaeb
A3aaan <andreas.vanderdonckt@student.kuleuven.be>
2023-03-11 01:49:52 +0100

    stopwoorden toegevoegd

--- NLTK/keywords.py
+++ NLTK/keywords.py
@@ -1,27 +1,22 @@
 # script.py
 from keybert import KeyBERT
 
-doc = """
-    Supervised learning is the machine learning task of learning a function that
-    maps an input to an output based on example input-output pairs. It infers a
-    function from labeled training data consisting of a set of training examples.
-    In supervised learning, each example is a pair consisting of an input object
-    (typically a vector) and a desired output value (also called the supervisory signal). 
-    A supervised learning algorithm analyzes the training data and produces an inferred function, 
-    which can be used for mapping new examples. An optimal scenario will allow for the 
-    algorithm to correctly determine the class labels for unseen instances. This requires 
-    the learning algorithm to generalize from the training data to unseen situations in a 
-    'reasonable' way (see inductive bias).
-"""
+f2 = open("stop_words_nederlands.txt",'r')
+stringSwords = f2.read()
+print(stringSwords)
+stringSwords = stringSwords.replace("\n"," ")
+swords = stringSwords.split(" ")
+print(swords)
+f2.close()
+input()
+
+
+file = open("opinion_file.txt",'r',encoding= 'iso-8859-1')
+doc = file.read()
+
+
 
 kw_model = KeyBERT()
 keywords = kw_model.extract_keywords(doc)
 
-print(kw_model.extract_keywords(doc, keyphrase_ngram_range=(1, 1), stop_words=None))
-#[
-#    ('learning', 0.4604),
-#    ('algorithm', 0.4556),
-#    ('training', 0.4487),
-#    ('class', 0.4086),
-#    ('mapping', 0.3700)
-#]
+print(kw_model.extract_keywords(doc, keyphrase_ngram_range=(2, 3), stop_words=swords))
